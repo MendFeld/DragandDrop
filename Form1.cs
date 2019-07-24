041d2b836580d278d7faafe1a421ced98e743d52
MendFeld <53190630+MendFeld@users.noreply.github.com>
2019-07-24 10:26:23 -0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DragDrop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ControlExtension.Draggable(button1, true);
+            ControlExtension.Draggable(textBox1, true);
+            ControlExtension.Draggable(comboBox1, true);
+            ControlExtension.Draggable(checkBox1, true);
+            ControlExtension.Draggable(dataGridView1, true);
+            ControlExtension.Draggable(progressBar1, true);
+        }
+    }
+}
